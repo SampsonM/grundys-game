eb01ca5c9f2c48d10810ac9f746eec6bc312e9fb
SampsonM <SampsonM@github.com>
2021-02-14 12:42:59 +0000

    Added logic to find best next move

--- py-serv/app.py
+++ py-serv/app.py
@@ -1,15 +1,47 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import math
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-# returns best position
-# positions - a string of positions in game
-# player - the player who's turn it is to move
-def get_best_possible_position(positions, player):
-	# if no positions can be split player past is loser
-	# if positions can be split, split one and recurse
+# todo
+# figure out which next move is best for current player
+# track which players move the split it
+# track if the current player wins/loses
+# track the turn which allows the current player to win
+	
+
+def get_best_possible_position(positions, current_player, orig_player):
+	# if no positions can be split, current player is loser
+	# if positions can be split, split one and recurse to find loser
+
+	next_player = 1 if current_player == 2 else 2
+	result = False
+	new_positions = ''
+	
+	for current_pos in positions.split(','):
+		p = int(current_pos)
+
+		if p > 2:
+			stop_pos = math.floor(p/2)
+
+			# for loop decrement -1 each time until halfway down
+			for i in range(p-1, stop_pos, -1):
+				bar = f"{i},{p-i}"
+				new_positions = positions.replace(current_pos,  bar)
+				result = get_best_possible_position(new_positions, next_player, orig_player)
+
+				if result[0] == orig_player:
+					break
+
+	# no split happened
+	if result == False:
+		return [current_player, new_positions]
+	else:
+		return [result[0], new_positions]		
+
+
 
 @app.route('/get-best-move', methods=['POST'])
 def collection():
@@ -17,7 +49,7 @@ def collection():
 		data = request.get_json()
 
 		if data and data['positions']:
-			pos = get_best_possible_position(data['positions'], 1)
+			pos = get_best_possible_position(data['positions'], 1, 1)
 			return jsonify(pos)
 		else:
 			return jsonify('No inputs found')
